fbc7ce81ad9b72ce657c654a5500dff4308ea041
Prasann Deshpande(Berlin) <pdeshpa2@binghamton.edu>
2021-04-06 02:31:12 -0400

    Greetings added!

--- app/src/main/java/com/app/puffcounter/MainActivity.java
+++ app/src/main/java/com/app/puffcounter/MainActivity.java
@@ -10,14 +10,20 @@ import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
     private static final String PREFS_NAME = "puffs";
     FloatingActionButton single;
     FloatingActionButton ten;
 
     TextView tv ;
+    TextView greet ;
     TextView total ;
     SharedPreferences settings;
+    List<String> welcome;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +31,23 @@ public class MainActivity extends AppCompatActivity {
 
         single = findViewById(R.id.one);
         ten = findViewById(R.id.ten);
-
+        greet = findViewById(R.id.greet);
         tv = findViewById(R.id.count);
         total = findViewById(R.id.total);
        settings = getSharedPreferences(PREFS_NAME, 0);
 
             total.setText(String.valueOf(settings.getInt("ct",0)));
-
+            welcome=new ArrayList();
+        welcome.add("Rock On!");welcome.add("Have a good one!");welcome.add("Great weather!");welcome.add("Beunos Dias!");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
+
+        Random a = new Random();
+        int r = a.nextInt(4);
+        greet.setText(welcome.get(r));
         single.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
